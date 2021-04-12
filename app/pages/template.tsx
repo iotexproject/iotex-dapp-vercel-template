8b43555d0d85919a72f39be3f026f4d3a45bb5c0
uuz <ncysatnaf@gmail.com>
2021-04-12 09:56:10 +0800

    remove temp code

--- app/pages/template.tsx
+++ app/pages/template.tsx
@@ -2,22 +2,10 @@ import { BlitzPage, invoke } from "blitz"
 import { observer } from "mobx-react-lite"
 import Layout from "app/layouts/Layout"
 import { useStore } from "app/stores"
-import templateMutation from "app/mutations/templateMutation"
-import templateQuery from "app/queries/templateQuery"
-import { useEffect } from "react"
-import { message } from "antd"
 
 const Teamplate: BlitzPage = observer(() => {
   const { lang } = useStore()
 
-  useEffect(() => {
-    invoke(templateQuery, { foo: "123" }).then(console.log)
-    invoke(templateMutation, { name: "test", password: "test" })
-      .then(console.log)
-      .catch((e) => {
-        console.log(123, { e })
-      })
-  }, [])
   return (
     <div>
       <div>{lang.t("hello", { message: "nextjs" })}</div>
